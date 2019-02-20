c389703a10949483dcaf296614d66564e2cac172
Grant Thompson <grantthompson@Grants-MBP.fios-router.home>
2019-02-20 16:53:59 -0500

    Camera Working Smoothly

--- Assets/Slingshot.cs
+++ Assets/Slingshot.cs
@@ -76,7 +76,7 @@ public class Slingshot : MonoBehaviour
             aimingMode = false;
             projectile.GetComponent<Rigidbody>().isKinematic = false;
             projectile.GetComponent<Rigidbody>().velocity = -mouseDelta * velocityMult;
-
+            FollowCam.POI = projectile;
             projectile = null;
            
 
